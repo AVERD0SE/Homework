6b134b210be624f08473cc5d5f20cfd9d2d45cbc
DmPC <averdose88@gmail.com>
2022-11-22 16:27:19 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (b > max) max = b;
+else Console.Write("max = ");
+
+Console.WriteLine(max);
