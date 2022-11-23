9885b7a7e2809055bc2c4e768b69ce1d269ed4ff
DmPC <averdose88@gmail.com>
2022-11-24 01:01:39 +0300

    commit Task6

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,15 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 
-//Console.Clear();
-
-//Console.WriteLine("Введи трёхзначное число: ");
-
-//int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
-//string stringNumber = Convert.ToString(ThreeDigitNumber);
-
-//Console.WriteLine("Вторая цифра введённого числа: " +stringNumber[1]);
-
 Console.WriteLine("Введите трехзначное число: ");
 string input = Console.ReadLine();
 int stringlen = 3;
--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Напишите программу, которая выводит третью цифру 
 //заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите число: ");
-string input = Console.ReadLine();
+//Console.WriteLine("Введите число: ");
+//string input = Console.ReadLine();
 //int stringlen = 3;
 //int inputlen = input.Length;
-for (int i = 0; i <= i + 2; i++)
+//for (int i = 0; i <= inputlen; i++)
+//{
+//    Console.WriteLine("{0}->{1}",input, input[2]);
+//    break;
+//}
+//Console.WriteLine("Третьей цифры нет");
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int inputlen = input.Length;
+int index = 2;
+if (inputlen >= index + 1)
 {
-    Console.WriteLine("{0}->{1}",input, input[2]);
-    break;
+    Console.WriteLine("{0}->{1}",input, input[index]);
 }
-Console.WriteLine("Третьей цифры нет");
-
-//    while(input < inputlen)
-//    {
-//        if(inputlen == stringlen)
-//            Console.WriteLine("{0}->{1}",input, input[1]);
-//            break;
-//    }
-
-//int N = int.Parse(Console.ReadLine());
-    
-//for (int i = 1; i <= N; i++)
-//    if (i % 2 == 0)
-//        Console.WriteLine(i);
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
