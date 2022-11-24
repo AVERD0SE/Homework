9dca1410589c9436771996560681623b7408d84d
DmPC <averdose88@gmail.com>
2022-11-24 14:04:36 +0300

    commit Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным
+
+int dayNumber = 7;
+if (1 <= dayNumber & dayNumber <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+if (6 <= dayNumber & dayNumber <= 7)
+{
+    Console.WriteLine("Выходной день");
+}
+if (dayNumber > 7)
+{
+    Console.WriteLine("Введено некорректное число");
+}
