45fe426990c0843f20fdc9b3842a464ec5d1ecf5
DmPC <averdose88@gmail.com>
2022-11-25 16:42:45 +0300

    add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+
+int Exp(int A, int B)
+{
+  int result = 1;
+  for(int i=1; i <= B; i++)
+  {
+    result = result * A;
+  }
+    return result;
+}
+  Console.Write("Введите число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  
+  Console.Write("Введите число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exp(A, B);
+  Console.WriteLine("A ^ B = " + exponentiation);
