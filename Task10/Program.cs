d649bc4422cd799c4e97317767b4930f3cc3b780
DmPC <averdose88@gmail.com>
2022-11-25 17:38:58 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int Sum = 0;
+
+for (int i=3; i>=0; i--)
+{
+    Sum += N / (int)Math.Pow (10.0,i);
+    N=N % (int)Math.Pow (10.0,i);
+}
+
+Console.WriteLine("Сумма всех цифр введённого числа: " + Sum);
