5fa4187f4b4349c1516459eab417ee9814f1dd8b
DmPC <averdose88@gmail.com>
2022-11-25 17:44:08 +0300

    add Task11

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -7,7 +7,7 @@ int Sum = 0;
 for (int i=3; i>=0; i--)
 {
     Sum += N / (int)Math.Pow (10.0,i);
-    N=N % (int)Math.Pow (10.0,i);
+    N = N % (int)Math.Pow (10.0,i);
 }
 
 Console.WriteLine("Сумма всех цифр введённого числа: " + Sum);
