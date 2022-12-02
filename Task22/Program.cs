61fcf922d3384a020eb0cdb9b82f13a6db0b0598
DmPC <averdose88@gmail.com>
2022-12-03 00:18:25 +0300

    add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = InputInt("Введите M: ");
+int N = InputInt("Введите N: ");
+Console.WriteLine($"Сумма натуральных элементов от {M} до {N} = {CountNaturalSum(M, N)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    return N + CountNaturalSum(M, N - 1);
+}
